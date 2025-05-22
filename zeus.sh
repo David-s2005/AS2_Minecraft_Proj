fac4943ea5940b02f1d3c9e9f510acc228d223ce
David <134564567+David-s2005@users.noreply.github.com>
2025-05-22 14:40:25 +0800

    Update zeus.sh

--- zeus.sh
+++ zeus.sh
@@ -4,6 +4,6 @@ zeus_phrase="I AM ZEUS, FEEL MY POWER!"
 
 for ((i=0; i<5; i++ ))
 do
-    mcrcon -H 13.237.63.199 -P 25575 -p 'Rigby1$1Rigby1$1' "execute as @a at @s run summon minecraft:limcrcon -H 13.237.63.199 -P 25575 -p 'Rigby1$1Rigby1$1' "say $zeus_phrase"
+    mcrcon -H 13.237.63.199 -P 25575 -p 'Password' "execute as @a at @s run summon minecraft:limcrcon -H 13.237.63.199 -P 25575 -p 'Rigby1$1Rigby1$1' "say $zeus_phrase"
     sleep 5
 done
